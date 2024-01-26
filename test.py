fbf6c518a7b6371bc282fb4caf6ffe269b6711dc
fatmamath <massoufatima23@gmail.com>
2024-01-27 00:13:52 +0100

    mini projet with json .

--- test.py
+++ test.py
@@ -1,36 +1,6 @@
-# list = ["a", "e", "i", "o", "u"]
-# t = []
-# dect = {}
-# k = 0
-# m = 0
-# # const = 'ooooocccococococococococoooooccc'
-# const = input(' -----> entrer la const :  ')
-# for i in range(len(const)):
-#     if const[i] == "o" :
-#         k += 1
-#     if const[i] == "c" :
-#         m += 1
-# dect['o'] = k
-# dect['m'] = m
-# print('dect = ' , dect)
-
-# # v = input("entrer voyelles : ")
-# # chain = input("entrer chain : ")
-# # chain2 =" "
-# # for i in range(len(chain)):
-# #     t.append(chain[i])
-# # print('t = ',t)
-# # for i in range(len(t)):
-# #     if t[i] in list:
-# #         t[i] = v
-# # print('t = ',t)
-# # for i in range(len(t)):
-# #     chain2 += t[i] 
-# # print('chain2 = ',chain2)
-
-# **********************************************************************
 import random
 import json
+
 fichier = open('testpython.json' , 'r')
 list = json.loads(fichier.read( ))
 print(list)
